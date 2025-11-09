3b09719f90d448bb8b2fc7109a1011c2bab5ec9e
mmsoliman21 <mohamedsoliman21@gmx.de>
2025-11-09 17:10:38 +0100

    Adding the new package

---
+++ src/emailapp/Emailapp.java
@@ -0,0 +1,9 @@
+package emailapp;
+
+public class Emailapp {
+    public static void main(String [] args){
+        Email em1 = new Email("Karl","Marx");
+        System.out.println(em1.showInfo());
+        System.out.println("Thank you!");
+    }
+}
